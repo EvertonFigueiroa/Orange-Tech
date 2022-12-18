92a011e7e3f71c73a26dc6100bb44e29d15ea459
EvertonFigueiroa <evertonbpg@gmail.com>
2022-12-18 15:42:25 -0300

    Criando a tela de cadastro DIO e algumas interação entre as páginas.

--- React/src/pages/cadastro/index.tsx
+++ React/src/pages/cadastro/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigate  } from "react-router-dom";
-import { MdEmail, MdLock } from 'react-icons/md'
+import { MdAddAPhoto, MdEmail, MdLock, MdOutlinePeopleOutline, MdPeople, MdPeopleAlt, MdPeopleOutline, MdPerson } from 'react-icons/md'
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
@@ -9,14 +9,14 @@ import { IFormData } from './types'
 import { useForm } from "react-hook-form";
 
 
-import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
+import { Container, Title, Column, TitleCadastro, SubtitleCadastro, SubText, CriarText, Row, Wrapper, CriarText2 } from './styles';
 
-const Login = () => {
+const Cadastro = () => {
 
     const navigate = useNavigate()
 
     const handleClickSignIn = () => {
-        navigate('/cadastro')
+        navigate('/login')
     }
 
     const { control, handleSubmit, formState: { errors  } } = useForm<IFormData>({
@@ -45,23 +45,27 @@ const Login = () => {
         <Header />
         <Container>
             <Column>
-                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
-                 e entrar mais rápido nas empresas mais desejadas.</Title>
+                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias e entrar mais rápido nas empresas mais desejadas.</Title>
             </Column>
             <Column>
                 <Wrapper>
-                <TitleLogin>Faça seu cadastro</TitleLogin>
-                <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                <TitleCadastro>Comece agora grátis</TitleCadastro>
+                <SubtitleCadastro>Crie sua conta e make the change._</SubtitleCadastro>
                 <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input placeholder="Nome completo" leftIcon={<MdPerson />} name="nome" control={control} />
+                    {errors.nameFull && <span>Nome completo é obrigatório</span>}
                     <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
                     {errors.email && <span>E-mail é obrigatório</span>}
                     <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
                     {errors.password && <span>Senha é obrigatório</span>}
-                    <Button title="Entrar" variant="secondary" type="submit"/>
+                    <Button title="Criar minha conta" variant="secondary" type="submit"/>
                 </form>
                 <Row>
-                    <EsqueciText>Esqueci minha senha</EsqueciText>
-                    <CriarText onClick={handleClickSignIn}>Criar Conta</CriarText>
+                    <SubText>Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas de Privacidade e os Termos de Uso da DIO.</SubText>
+                </Row>
+                <Row>
+                    <CriarText>Já tenho conta.</CriarText>
+                      <CriarText2 onClick={handleClickSignIn}>Fazer login</CriarText2>
                 </Row>
                 </Wrapper>
             </Column>
@@ -69,4 +73,4 @@ const Login = () => {
     </>)
 }
 
-export { Login }
+export { Cadastro }
--- React/src/pages/cadastro/styles.ts
+++ React/src/pages/cadastro/styles.ts
@@ -22,7 +22,6 @@ export const Column = styled.div`
 export const Row = styled.div`
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
     align-items: center;
     margin-top: 20px;
 `
@@ -40,16 +39,16 @@ export const Title = styled.h2`
     color: #FFFFFF;
 `
 
-export const TitleLogin = styled.p`
+export const TitleCadastro = styled.p`
     font-family: 'Open Sans';
     font-style: normal;
     font-weight: 700;
-    font-size: 32px;
+    font-size: 29px;
     line-height: 44px;
     margin-bottom: 8px;
 `
 
-export const SubtitleLogin = styled.p`
+export const SubtitleCadastro = styled.p`
     font-family: 'Open Sans';
     font-style: normal;
     font-weight: 400;
@@ -59,31 +58,34 @@ export const SubtitleLogin = styled.p`
 `
 
 
-export const EsqueciText  = styled.p`
+export const SubText = styled.p`
     font-family: 'Open Sans';
     font-style: normal;
-    font-weight: 700;
-    font-size: 14px;
-    line-height: 19px;
-    color: #E5E044;
-    cursor: pointer;
-    
-    :hover {
-        text-decoration: underline;
-    }
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 25px
 ` 
 
 export const CriarText =  styled.p`
-    font-family: 'Open Sans';
+    font-family: "Open Sans";
     font-style: normal;
     font-weight: 700;
     font-size: 14px;
     line-height: 19px;
-    color: #23DD7A;
+    color: #ffffff;
+`   
+
+export const CriarText2 =  styled.p`
+    font-family: "Open Sans";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+    margin-left: 5px;
+    color: #23dd7a;
     cursor: pointer;
     
     :hover {
         text-decoration: underline;
     }
-
 `
